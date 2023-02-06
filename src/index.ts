f62dbff24fc633c899d7fa041156eb035fd84da4
Kristopher <kristopher_matic@hotmail.com>
2023-02-06 13:26:31 -0800

    basic server commands functionality

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import * as dotenv from "dotenv";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
 dotenv.config();
 
 const token = process.env.BOT_TOKEN;
 
-console.log(token);
+// create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);

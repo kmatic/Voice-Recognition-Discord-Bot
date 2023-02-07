cf60b7f3005f63c2e5725ca8b5024158ba37c6ce
Kristopher <kristopher_matic@hotmail.com>
2023-02-06 17:27:50 -0800

    clean up commands exports

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,9 +1,8 @@
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with Pong!");
-
-export async function execute(interaction: CommandInteraction): Promise<void> {
-    await interaction.reply("Pong!");
-}
+export default {
+    data: new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
+    async execute(interaction: CommandInteraction): Promise<void> {
+        await interaction.reply("Pong!");
+    },
+};
--- src/commands/server.ts
+++ src/commands/server.ts
@@ -1,12 +1,13 @@
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-    .setName("server")
-    .setDescription("Provides information about the server.");
-
-export async function execute(interaction: CommandInteraction) {
-    // interaction.guild is the object representing the Guild in which the command was run
-    await interaction.reply(
-        `This server is ${interaction.guild?.name} and has ${interaction.guild?.memberCount} members.`
-    );
-}
+export default {
+    data: new SlashCommandBuilder()
+        .setName("server")
+        .setDescription("Provides information about the server."),
+    async execute(interaction: CommandInteraction): Promise<void> {
+        // interaction.guild is the object representing the Guild in which the command was run
+        await interaction.reply(
+            `This server is ${interaction.guild?.name} and has ${interaction.guild?.memberCount} members.`
+        );
+    },
+};

2741c1345b33f1231edd7140f269eb6e65d66d21
Kristopher <kristopher_matic@hotmail.com>
2023-03-06 21:16:37 -0800

    add speech contexts

--- src/utils/transcribeAudio.ts
+++ src/utils/transcribeAudio.ts
@@ -1,10 +1,13 @@
-// import speech from "@google-cloud/speech";
-
 export default async function transcribeAudio(inputAudio: Buffer, client: any) {
     const config = {
         encoding: "LINEAR16",
         sampleRateHertz: 16000,
         languageCode: "en-us",
+        speech_contexts: [
+            {
+                phrases: ["play", "skip", "pause", "resume", "disconnect"],
+            },
+        ],
     };
 
     const audio = {
@@ -16,7 +19,7 @@ export default async function transcribeAudio(inputAudio: Buffer, client: any) {
         config: config,
     };
 
-    const [response] = await client.recognize(request);
+    const [response]: any = await client.recognize(request);
     const transcription = response.results
         .map((result: any) => result.alternatives[0].transcript)
         .join("\n");
